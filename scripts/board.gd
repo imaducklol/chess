75d680b65b7db30d903ec0593d94a2498b93c7c0
Orion Hess <orionhess@gmail.com>
2024-12-13 12:14:23 -0800

    Basic board_eval() done It currently just returns material advantage, but its a start

--- scripts/board.gd
+++ scripts/board.gd
@@ -53,6 +53,7 @@ func move(src: int, dest: int) -> void:
 	board[dest] = board[src]
 	board[src] = Piece.new()
 	turn = !turn
+	print(board_eval(board))
 
 func get_moves(pos: int) -> Array[int]:
 	var piece := board[pos]
@@ -75,5 +76,53 @@ func get_moves(pos: int) -> Array[int]:
 			return move_generation.rook_moves(piece, pos)
 	return []
 
-func board_eval() -> float:
+func board_eval(board_to_eval: Array[Piece]) -> float:
+	var eval := 0.0
+	eval += pieces_value(board_to_eval)
+	return eval
+	
+func pieces_value(board_to_eval: Array[Piece]) -> float:
+	var value := 0.0
+	for i in range(0, 64):
+		var piece_value := 0.0
+		var piece := board_to_eval[i]
+		match piece.type:
+			Piece.Type.NONE:
+				continue
+			Piece.Type.PAWN:
+				piece_value = 1
+			Piece.Type.KNIGHT:
+				piece_value = 3
+			Piece.Type.BISHOP:
+				piece_value = 3
+			Piece.Type.ROOK:
+				piece_value = 5
+			Piece.Type.QUEEN:
+				piece_value = 9
+			Piece.Type.KING:
+				piece_value = 200
+		
+		if piece.team == Piece.Team.BLACK:
+			piece_value *= -1
+		
+		value += piece_value
+	return value
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
